cbf7edae1b61d0aac260ad458fcedfa7b74681e0
austintyx <atyx_austinteo@outlook.com>
2025-10-18 23:08:38 +0800

    remove photos

--- algo/simulator/src/components/misc/PageFooter.tsx
+++ algo/simulator/src/components/misc/PageFooter.tsx
@@ -4,16 +4,7 @@ import { FaGithub } from "react-icons/fa";
 export const PageFooter = () => {
   return (
     <footer className="flex justify-center gap-2">
-      <div>&copy; AY24/25 Sem 2 Group 17</div>
-      {/* <div>|</div>
-      <a
-        href="https://github.com/xJQx/SC2079-MDP-Group11-Algorithms"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="cursor-pointer hover:text-black"
-      >
-        <FaGithub className="w-[24px] h-[24px]" />
-      </a> */}
+      <div>AY25/26 Sem 1 Group 40</div>
     </footer>
   );
 };
--- algo/simulator/src/components/misc/PageHeader.tsx
+++ algo/simulator/src/components/misc/PageHeader.tsx
@@ -4,8 +4,6 @@ import { TypeAnimation } from "react-type-animation";
 export const PageHeader = () => {
   return (
     <div className="flex flex-col items-center justify-center">
-      {/* Logo */}
-      <img src="/logo.png" alt="logo" />
 
       {/* Title */}
       <div className="font-bold text-[42px] text-center">
